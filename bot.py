7ab4e30a9e1b5f812161d2e666ac50ee719713be
moonholiday <727moonholiday@gmail.com>
2020-10-03 10:24:55 +0545

    responds to 69

--- bot.py
+++ bot.py
@@ -1,15 +1,38 @@
 # bot.py
 import os
 import discord
+import random
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-class CustomClient(discord.Client):
-    async def on_ready(self):
-        print(f'{self.user} has been connected to Discord')
-client = CustomClient()
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user.name} is now connected to Discord!')
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, Vsdk welcome to our server!'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    res = [
+         'Nice!!', 'VSDK', 'Hot funeral selfies', 'I slay it, Queen'
+    ]
+
+    if message.content == '69':
+        response = random.choice(res)
+        await message.channel.send(response)
+
 client.run(TOKEN)
 
 
